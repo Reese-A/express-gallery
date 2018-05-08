21f4f01d70fb3259542b8212307c5fe39aa77adc
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-07 16:13:44 -1000

    setup handlebars and gallery/new route

--- routes/gallery/index.js
+++ routes/gallery/index.js
@@ -3,4 +3,10 @@ const Gallery = require('../../db/models/Gallery');
 
 const router = express.Router();
 
+
+router.route('/new')
+  .get((req,res) => {
+    return res.render('gallery/new');
+  });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 
 const app = express();
+
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
+
+app.use(methodOverride('_method'))
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
